a6082d0951f04fafeb13ea6ece8da21b7dcd594b
Lucifermo90 <163461353+Lucifermo90@users.noreply.github.com>
2024-09-26 16:34:37 +0530

    Add files via upload

--- YukkiMusic/utils/decorators/play.py
+++ YukkiMusic/utils/decorators/play.py
@@ -1,39 +1,74 @@
 #
-# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2021 by TeamYukki@Github, < https://github.com/TeamYukki >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# This file is part of < https://github.com/TeamYukki/YukkiMusic > project,
+# and is released under the MIT License.
+# Please see < https://github.com/TeamYukki/YukkiMusic/blob/master/LICENSE >
 #
 # All rights reserved.
 #
 
+import asyncio
+
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.errors import (
+    ChatAdminRequired,
+    InviteRequestSent,
+    UserAlreadyParticipant,
+    UserNotParticipant,
+)
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from config import PLAYLIST_IMG_URL, PRIVATE_BOT_MODE, adminlist
+from config import PLAYLIST_IMG_URL, PRIVATE_BOT_MODE
+from config import SUPPORT_GROUP as SUPPORT_CHAT
+from config import adminlist
 from strings import get_string
 from YukkiMusic import YouTube, app
+from YukkiMusic.core.call import Yukki
 from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import (get_cmode, get_lang,
-                                       get_playmode, get_playtype,
-                                       is_active_chat,
-                                       is_commanddelete_on,
-                                       is_served_private_chat)
-from YukkiMusic.utils.database.memorydatabase import is_maintenance
-from YukkiMusic.utils.inline.playlist import botplaylist_markup
+from YukkiMusic.utils.database import (
+    get_assistant,
+    get_cmode,
+    get_lang,
+    get_playmode,
+    get_playtype,
+    is_active_chat,
+    is_commanddelete_on,
+    is_maintenance,
+    is_served_private_chat,
+)
+from YukkiMusic.utils.inline import botplaylist_markup
+
+links = {}
 
 
 def PlayWrapper(command):
     async def wrapper(client, message):
+        language = await get_lang(message.chat.id)
+        _ = get_string(language)
+        if message.sender_chat:
+            upl = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="How to fix this ?",
+                            callback_data="AnonymousAdmin",
+                        ),
+                    ]
+                ]
+            )
+            return await message.reply_text(_["general_4"], reply_markup=upl)
+
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    "Bot is under maintenance. Please wait for some time..."
+                    text=f"{app.mention} is under maintenance.",
+                    disable_web_page_preview=True,
                 )
         if PRIVATE_BOT_MODE == str(True):
             if not await is_served_private_chat(message.chat.id):
                 await message.reply_text(
-                    "**Private Music Bot**\n\nOnly for authorized chats from the owner. Ask my owner to allow your chat first."
+                    "**Private Music Bot**\n\nOnly For Authorize Chats."
                 )
                 return await app.leave_chat(message.chat.id)
         if await is_commanddelete_on(message.chat.id):
@@ -41,30 +76,19 @@ def PlayWrapper(command):
                 await message.delete()
             except:
                 pass
-        language = await get_lang(message.chat.id)
-        _ = get_string(language)
+
         audio_telegram = (
-            (
-                message.reply_to_message.audio
-                or message.reply_to_message.voice
-            )
+            (message.reply_to_message.audio or message.reply_to_message.voice)
             if message.reply_to_message
             else None
         )
         video_telegram = (
-            (
-                message.reply_to_message.video
-                or message.reply_to_message.document
-            )
+            (message.reply_to_message.video or message.reply_to_message.document)
             if message.reply_to_message
             else None
         )
         url = await YouTube.url(message)
-        if (
-            audio_telegram is None
-            and video_telegram is None
-            and url is None
-        ):
+        if audio_telegram is None and video_telegram is None and url is None:
             if len(message.command) < 2:
                 if "stream" in message.command:
                     return await message.reply_text(_["str_1"])
@@ -74,20 +98,6 @@ def PlayWrapper(command):
                     caption=_["playlist_1"],
                     reply_markup=InlineKeyboardMarkup(buttons),
                 )
-        if message.sender_chat:
-            upl = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            text="How to Fix this? ",
-                            callback_data="AnonymousAdmin",
-                        ),
-                    ]
-                ]
-            )
-            return await message.reply_text(
-                _["general_4"], reply_markup=upl
-            )
         if message.command[0][0] == "c":
             chat_id = await get_cmode(message.chat.id)
             if chat_id is None:
@@ -100,6 +110,15 @@ def PlayWrapper(command):
         else:
             chat_id = message.chat.id
             channel = None
+        try:
+            is_call_active = (await app.get_chat(chat_id)).is_call_active
+            if not is_call_active:
+                return await message.reply_text(
+                    f"**No Active VideoChat Found.**\n\nPlease Make Sure You Started The VC."
+                )
+        except Exception:
+            pass
+
         playmode = await get_playmode(message.chat.id)
         playty = await get_playtype(message.chat.id)
         if playty != "Everyone":
@@ -123,6 +142,89 @@ def PlayWrapper(command):
             fplay = True
         else:
             fplay = None
+
+        if not await is_active_chat(chat_id):
+            userbot = await get_assistant(message.chat.id)
+            try:
+                try:
+                    get = await app.get_chat_member(chat_id, userbot.id)
+                except ChatAdminRequired:
+                    return await message.reply_text(_["call_1"])
+                if (
+                    get.status == ChatMemberStatus.BANNED
+                    or get.status == ChatMemberStatus.RESTRICTED
+                ):
+                    try:
+                        await app.unban_chat_member(chat_id, userbot.id)
+                    except:
+                        return await message.reply_text(
+                            text=_["call_2"].format(userbot.username, userbot.id),
+                        )
+            except UserNotParticipant:
+                if chat_id in links:
+                    invitelink = links[chat_id]
+                else:
+                    if message.chat.username:
+                        invitelink = message.chat.username
+                        try:
+                            await userbot.resolve_peer(invitelink)
+                        except:
+                            pass
+                    else:
+                        try:
+                            invitelink = await client.export_chat_invite_link(
+                                message.chat.id
+                            )
+                        except ChatAdminRequired:
+                            return await message.reply_text(_["call_1"])
+                        except Exception as e:
+                            return await message.reply_text(
+                                _["call_3"].format(app.mention, type(e).__name__)
+                            )
+
+                if invitelink.startswith("https://t.me/+"):
+                    invitelink = invitelink.replace(
+                        "https://t.me/+", "https://t.me/joinchat/"
+                    )
+                myu = await message.reply_text(_["call_5"])
+                try:
+                    await asyncio.sleep(1)
+                    await userbot.join_chat(invitelink)
+                except InviteRequestSent:
+                    try:
+                        await app.approve_chat_join_request(chat_id, userbot.id)
+                    except Exception as e:
+                        return await myu.edit(_["call_3"].format(type(e).__name__))
+                    await asyncio.sleep(1)
+                    await myu.edit(_["call_6"].format(app.mention))
+                except UserAlreadyParticipant:
+                    pass
+                except Exception as e:
+                    return await myu.edit(_["call_3"].format(type(e).__name__))
+
+                links[chat_id] = invitelink
+                try:
+                    await myu.delete()
+                except Exception:
+                    pass
+
+                try:
+                    await userbot.resolve_peer(chat_id)
+                except:
+                    pass
+                try:
+                    call_participants_id = [
+                        member.chat.id
+                        async for member in userbot.get_call_members(chat_id)
+                    ]
+                    if await is_active_chat(chat_id) and (
+                        not call_participants_id
+                        or userbot.id not in call_participants_id
+                    ):
+                        await Yukki.stop_stream(chat_id)
+                except:
+                    pass
+
         return await command(
             client,
             message,
